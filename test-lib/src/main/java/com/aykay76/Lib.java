de65369d95c3913b503f648ac9cc36b5101c91d3
Alan Kelly <alan.kelly@maersk.com>
2024-11-18 12:58:16 +0000

    first commit

---
+++ test-lib/src/main/java/com/aykay76/Lib.java
@@ -0,0 +1,7 @@
+package com.aykay76;
+
+public class Lib {
+    public static void SayHello() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ test-lib/src/test/java/com/aykay76/LibTest.java
@@ -0,0 +1,12 @@
+package com.aykay76;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class LibTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
